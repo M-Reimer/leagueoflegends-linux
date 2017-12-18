23e3a9fa6549b409c25bf7590edbf33543c5ef74
Manuel Reimer <manuel.reimer@gmx.de>
2017-12-18 14:12:42 +0100

    Remove need for root permissions

--- install.sh
+++ install.sh
@@ -29,12 +29,15 @@ mkdir -p $WINEPREFIX/drive_c/Riot\ Games/League\ of\ Legends/Config
 echo -e '[General]\nWindowMode=2' > $WINEPREFIX/drive_c/Riot\ Games/League\ of\ Legends/Config/game.cfg
 
 echo "*************************************************"
-echo "The next few steps will prompt you for shortcut creations. If root is required, please enter your root password when prompted."
+echo "The next few steps will prompt you for shortcut creations."
 echo "*************************************************"
 
 echo "*************************************************"
 echo "Creating League of Legends shell script"
 echo "*************************************************"
+
+# This is the "user local" BIN-directory for many distributions
+mkdir -p $HOME/bin
 
 echo "#!/bin/bash" > leagueoflegends.sh
 echo "export __GL_THREADED_OPTIMIZATIONS=1" >> leagueoflegends.sh
@@ -42,32 +45,30 @@ echo "export __GL_THREADED_OPTIMIZATIONS=1" >> leagueoflegends.sh
 echo "WINEARCH=win32 WINEPREFIX=\"$WINEPREFIX\" WINEDEBUG=-all wine C:/Riot\ Games/League\ of\ Legends/LeagueClient.exe" >> leagueoflegends.sh
 
 chmod a+x leagueoflegends.sh
-sudo cp leagueoflegends.sh /usr/bin/leagueoflegends
+cp leagueoflegends.sh $HOME/bin/leagueoflegends
 
 
 read -p "Would you like a menu shortcut? y/n" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-
 	echo "*************************************************"
 	echo "Creating League of Legends application menu shortcut."
 	echo "*************************************************"
-
-	sudo cp $WINEPREFIX/drive_c/Riot\ Games/League\ of\ Legends/RADS/system/lcu.ico /usr/share/pixmaps/leagueoflegends.ico
 
 	echo "[Desktop Entry]" > leagueoflegends.desktop
 	echo "Encoding=UTF-8" >> leagueoflegends.desktop
 	echo "Name=League of Legends" >> leagueoflegends.desktop
 	echo "GenericName=League of Legends" >> leagueoflegends.desktop
-	echo "Exec=/usr/bin/leagueoflegends \"\$@\"" >> leagueoflegends.desktop
-	echo "Icon=/usr/share/pixmaps/leagueoflegends.ico" >> leagueoflegends.desktop
+	echo "Exec=$HOME/bin/leagueoflegends \"\$@\"" >> leagueoflegends.desktop
+	echo "Icon=$WINEPREFIX/drive_c/Riot\ Games/League\ of\ Legends/RADS/system/lcu.ico /usr/share/pixmaps/leagueoflegends.ico" >> leagueoflegends.desktop
 	echo "StartupNotify=true" >> leagueoflegends.desktop
 	echo "Terminal=false" >> leagueoflegends.desktop
 	echo "Type=Application" >> leagueoflegends.desktop
 	echo "Categories=Application;Game" >> leagueoflegends.desktop
 
-	sudo cp leagueoflegends.desktop /usr/share/applications/
+	cp leagueoflegends.desktop $HOME/.local/share/applications/
+	update-desktop-database $HOME/.local/share/applications
 fi
 
 read -p "Would you like a desktop shortcut? y/n" -n 1 -r
@@ -77,7 +78,7 @@ then
 	echo "*************************************************"
 	echo "Creating League of Legends desktop shortcut."
 	echo "*************************************************"
-	cp /usr/share/applications/leagueoflegends.desktop /home/$USER/Desktop/
+	cp leagueoflegends.desktop $HOME/Desktop/
 fi
 
 
--- uninstall.sh
+++ uninstall.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 echo "*************************************************"
 echo "The next few steps will remove all shortcuts and league of legends files."
-echo "If root is required, please enter your root password when prompted."
 echo "*************************************************"
 
 echo "*************************************************"
-echo "Removing /usr/bin/leagueoflegends"
+echo "Removing $HOME/bin/leagueoflegends"
 echo "*************************************************"
-sudo rm -R /usr/bin/leagueoflegends
+sudo rm $HOME/bin/leagueoflegends
 
 echo "*************************************************"
-echo "Removing /usr/share/pixmaps/leagueoflegends.ico"
+echo "Removing $HOME/.local/share/applications/leagueoflegends.desktop"
 echo "*************************************************"
-sudo rm -R /usr/share/pixmaps/leagueoflegends.ico
+sudo rm $HOME/.local/share/applications/leagueoflegends.desktop
 
 echo "*************************************************"
-echo "Removing /usr/share/applications/leagueoflegends.desktop"
+echo "Removing $HOME/Desktop/leagueoflegends.desktop"
 echo "*************************************************"
-sudo rm -R /usr/share/applications/leagueoflegends.desktop 
+sudo rm $HOME/Desktop/leagueoflegends.desktop
 
 echo "*************************************************"
-echo "Removing sudo rm -R /home/$USER/Desktop/leagueoflegends.desktop"
+echo "Removing $HOME/League of Legends"
 echo "*************************************************"
-sudo rm -R /home/$USER/Desktop/leagueoflegends.desktop
-
-echo "*************************************************"
-echo "Removing home/$USER/League of Legends"
-echo "*************************************************"
-sudo rm -R /home/$USER/League\ of\ Legends
+sudo rm -R $HOME/League\ of\ Legends
 
 echo "*************************************************"
 echo "League of Legends has been successfully removed."
