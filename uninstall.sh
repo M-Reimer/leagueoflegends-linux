45ece033c30af587a7a56c51ba0cfcd1b71992ed
Manuel Reimer <manuel.reimer@gmx.de>
2019-10-24 19:52:57 +0200

    Fix uninstaller

--- uninstall.sh
+++ uninstall.sh
@@ -6,22 +6,22 @@ echo "*************************************************"
 echo "*************************************************"
 echo "Removing $HOME/bin/leagueoflegends"
 echo "*************************************************"
-sudo rm $HOME/bin/leagueoflegends
+rm $HOME/bin/leagueoflegends
 
 echo "*************************************************"
 echo "Removing $HOME/.local/share/applications/leagueoflegends.desktop"
 echo "*************************************************"
-sudo rm $HOME/.local/share/applications/leagueoflegends.desktop
+rm $HOME/.local/share/applications/leagueoflegends.desktop
 
 echo "*************************************************"
 echo "Removing $HOME/Desktop/leagueoflegends.desktop"
 echo "*************************************************"
-sudo rm $HOME/Desktop/leagueoflegends.desktop
+rm $HOME/Desktop/leagueoflegends.desktop
 
 echo "*************************************************"
-echo "Removing $HOME/League of Legends"
+echo "Removing $HOME/.wineprefix/LoL"
 echo "*************************************************"
-sudo rm -R $HOME/League\ of\ Legends
+rm -r $HOME/.wineprefix/LoL
 
 echo "*************************************************"
 echo "League of Legends has been successfully removed."
