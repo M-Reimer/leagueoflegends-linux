4cb17a01437c017f77e2eabdf938ef5c01f987b2
Manuel Reimer <manuel.reimer@gmx.de>
2020-10-03 11:31:36 +0200

    lol-launchhelper.sh: Add some comments

--- lol-launchhelper.sh
+++ lol-launchhelper.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+# Find PID
 process=LeagueClientUx.exe
 uxpid=$(timeout 2m sh -c "until pidof ${process}; do sleep 1; done")
 if [[ ! -n $uxpid ]]; then
   echo "Could not find process ${process}"
   exit 1
 fi
+
+# Get the app-port parameter
 echo "LeagueClientUx pid: ${uxpid}"
 port=$(xargs -0 < /proc/${uxpid}/cmdline \
   | sed -n 's/.*--app-port=\([[:digit:]]*\).*/\1/p')
@@ -14,6 +18,7 @@ if [[ ! -n $port ]]; then
 fi
 echo "Waiting for port ${port}"
 
+# Suspend the league client until app-port gets available
 kill -STOP ${uxpid}
 timeout 5m bash -c "
 until openssl s_client -connect :${port} <<< Q > /dev/null 2>&1; do
