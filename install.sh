5376b3fd6b305b36037d3e5b62592a42ac49ff61
GeorgScheumann <53917226+GeorgScheumann@users.noreply.github.com>
2020-10-18 17:43:34 +0200

    Make the lol-launchhelper script exectutable and run at start (#14)
    * Add files via upload
    
    * Update install.sh

--- install.sh
+++ install.sh
@@ -52,11 +52,12 @@ echo "export __GL_THREADED_OPTIMIZATIONS=1" >> leagueoflegends.sh
 echo "export MESA_GLTHREAD=TRUE" >> leagueoflegends.sh
 echo "export GPU_MAX_HEAP_SIZE=100" >> leagueoflegends.sh
 echo "export GPU_MAX_ALLOC_PERCENT=100" >> leagueoflegends.sh
-
+echo "bash $HOME/bin/lol-launchhelper &" >> leagueoflegends.sh
 echo "WINEARCH=win32 WINEPREFIX=\"$WINEPREFIX\" WINEDEBUG=-all /opt/wine-lol/bin/wine \"C:/Riot Games/League of Legends/LeagueClient.exe\"" >> leagueoflegends.sh
 
 chmod a+x leagueoflegends.sh
 cp leagueoflegends.sh "$HOME/bin/leagueoflegends"
+cp lol-launchhelper.sh "$HOME/bin/lol-launchhelper"
 
 echo "*************************************************"
 echo "Generating League of Legends .desktop file"
